44212c9c59a660b272e5f5f5198062d9154b4660
Francis Ngo <francis.t.ngo@gmail.com>
2023-10-23 22:10:44 -0700

    Add index.html and css

---
+++ script.js
@@ -0,0 +1,50 @@
+const list = [2, 4, 5, 2, 3, 4];
+
+function buildObjectBySeenCount(list) {
+	const result = {};
+
+	list.forEach((item) => {
+		if (!result[item]) {
+			result[item] = 0;
+		}
+
+		result[item] += 1;
+	});
+
+	return result;
+}
+
+function buildHistogram() {
+	const data = buildObjectBySeenCount(list);
+	const leftAxis = document.querySelectorAll(".left-axis")[0];
+	const bottomAxis = document.querySelectorAll(".bottom-axis")[0];
+	const contentAxis = document.querySelectorAll(".content")[0];
+	const leftAxisValues = [];
+
+	for (const key in data) {
+		const val = data[key];
+
+		if (leftAxisValues.indexOf(val) === -1) {
+			leftAxisValues.push(val);
+			const keyElement = document.createElement("div");
+			keyElement.textContent = val;
+			leftAxis.append(keyElement);
+		}
+	}
+
+	for (const key in data) {
+		const val = data[key];
+
+		const keyElement = document.createElement("div");
+		keyElement.textContent = key;
+
+		const valElement = document.createElement("div");
+		valElement.style.height =
+			(val / Math.max(...leftAxisValues)) * 100 + "%";
+
+		bottomAxis.append(keyElement);
+		contentAxis.append(valElement);
+	}
+}
+
+buildHistogram();
